a37401f1999f04f1cbfce8afdbfd095758c99de1
FrancescoAdesso4 <adesso.francesco@iisgalvanimi.edu.it>
2024-04-25 12:03:09 +0000

    abbellimento five-days

--- MiniMeteo/src/app/five-days/five-days.component.ts
+++ MiniMeteo/src/app/five-days/five-days.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { APICallsService} from '../apicalls.service';
+import { APICallsService } from '../apicalls.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,17 +12,27 @@ export class FiveDaysComponent {
   title = 'first-routed-app';
   obsCW: Observable<Object>;
   results: any;
-  
+  selectedIndex: number = 0;
+
   constructor(public call: APICallsService) { }
 
-  submit(query:HTMLInputElement): void {
+  submit(query: HTMLInputElement): void {
     if (!query.value) {
       return;
     }
     this.query = query.value;
     this.obsCW = this.call.search5D(this.query);
-    this.obsCW.subscribe((data) => this.results = data); 
-    
+    this.obsCW.subscribe((data) => {
+      this.results = data;
+      this.selectedIndex = 0; // Reset selectedIndex when new data is fetched
+    });
   }
-  
+
+  nextItem() {
+    this.selectedIndex = (this.selectedIndex === this.results.list.length - 1) ? 0 : this.selectedIndex + 1;
+  }
+
+  prevItem() {
+    this.selectedIndex = (this.selectedIndex === 0) ? this.results.list.length - 1 : this.selectedIndex - 1;
+  }
 }
